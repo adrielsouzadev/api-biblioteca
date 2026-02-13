3e9329244f4b36541f3f27ecfba817a5d0eb08b8
Adriel Souza <adrielsouza2023@outlook.com>
2026-02-12 22:38:49 -0300

    configurar servidor express e rotas

---
+++ scr/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+import livros from './routes/livros.routes.js';
+import usuarios from './routes/usuarios.routes.js';
+import empresitmos from './routes/emprestimos.routes'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(livros);
+app.use(usuarios);
+app.use(empresitmos);
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
