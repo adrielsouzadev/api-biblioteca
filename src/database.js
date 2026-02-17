2805d5a37bc9f074470a4ed613354ac49da32d18
Adriel Souza <adrielsouza2023@outlook.com>
2026-02-16 23:36:20 -0300

    feat: configuração da conexão assíncrona com mysql2

--- src/database.js
+++ src/database.js
@@ -1,10 +1,17 @@
 import mysql from 'mysql2/promise';
 
 export async function conectar() {
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'biblioteca'
-    });
+    try {
+        const conexao = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1234',
+            database: 'biblioteca'
+        });
+
+        return conexao;
+    } catch (erro) {
+        console.error('Erro ao conectar ao banco de dados:', erro.message);
+        throw erro;
+    }
 }
